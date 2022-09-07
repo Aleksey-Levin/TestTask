e88b19bf02bbe4f1894ea8da97f1d91d869003ab
LewinUp <raft.took@yandex.ru>
2022-09-08 00:22:02 +0300

    fix: problem with invalid value

--- js/changeCircle.js
+++ js/changeCircle.js
@@ -2,16 +2,21 @@ import InvalidCircleValueException from "./Exceptions/InvalidCircleValue.js";
 
 export default function changeCircle(currentValue, newValue) {
     let circularProgress = document.querySelector('.circular-progress');
-    if (newValue > 100 || newValue < 0) { throw new InvalidCircleValueException(newValue); }
+    try{
+        if (newValue > 100 || newValue < 0) { throw new InvalidCircleValueException(newValue);}
 
-    let value = currentValue;
+        let value = currentValue;
 
-    let progress = setInterval(() => {
-        if (value == newValue - 1 || value == newValue + 1 || value == newValue) { clearInterval(progress);}
+        let progress = setInterval(() => {
+            if (value == newValue - 1 || value == newValue + 1 || value == newValue) { clearInterval(progress);}
 
-        if (value < newValue) {value++; console.log("UP");}
-        if (value > newValue) value--;
+            if (value < newValue) value++;
+            if (value > newValue) value--;
 
-        circularProgress.style.background = `conic-gradient(#0058f0 ${value * 3.6}deg, #eef2f5 0deg)`
-    }, 2);
+            circularProgress.style.background = `conic-gradient(#0058f0 ${value * 3.6}deg, #eef2f5 0deg)`
+        }, 2);
+    }
+    catch (e) {
+        alert("Invalid value");
+    }
 }
